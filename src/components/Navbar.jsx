c3b6529f08551117d7ca967e05c736eee28cda40
GripsonTH <gripsonthongam@gmail.com>
2025-04-17 20:38:28 +0530

    first

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from "../assets/GTLogo.png";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import Resume from "../assets/Resume.png"
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/"><img className="mx-2 w-20" src={logo} alt="logo" /></a>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/thongam-gripson-695185271/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="hover:text-blue-600 transition duration-200" />
+        </a>
+        <a
+          href="https://github.com/GripsonTH"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="hover:text-gray-500 transition duration-200" />
+        </a>
+        <a
+          href="https://www.instagram.com/gripson_thongam/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram className="hover:text-pink-500 transition duration-200" />
+        </a>
+        <a href="/GripsonResume.pdf" download="GripsonResume.pdf"><img className="w-7 hover:scale-105 hover:rotate-3 hover:opacity-75 transition duration-200" src={Resume} alt="Resume" /></a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,92 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+import project5 from "../assets/projects/project-5.webp";
+import project6 from "../assets/projects/project-6.jpeg";
+import project7 from "../assets/projects/project-7.png";
+
+
+export const HERO_CONTENT = `I am an aspiring full stack developer with a passion for building robust and scalable web applications. With a solid foundation in front-end technologies like React, along with back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB, I am eager to apply my knowledge to real-world projects. I am continuously learning and refining my skills to create innovative solutions that enhance user experiences and drive business growth.`;
+
+export const ABOUT_TEXT = `I am a dedicated and passionate Full Stack Developer with a strong foundation in building dynamic and scalable web applications. My expertise spans React, Node.js, MySQL, PostgreSQL and MongoDB, allowing me to craft seamless and efficient digital experiences.
+
+Beyond web development, I have ventured into game development, where I built an online multiplayer shooting game for PC using Unity (C#) and Photon PUN2. Additionally, I have worked on several Python projects, exploring automation, data processing and backend development.
+
+My journey in technology is driven by curiosity and a love for problem-solving. I thrive in collaborative environments and continuously seek opportunities to learn and improve. When I’m not coding, I enjoy experimenting with new technologies, contributing to open-source projects and refining my game development skills.
+
+`;
+
+export const EXPERIENCES = [
+  /*{
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },*/
+  {
+    year: "2022 jan - jun",  
+    role: "IT Trainee",
+    company: " PIIMA Health Care",
+    description: `I worked as an IT Trainee at a Hospital for 6 months. During my time there, I was responsible for a variety of tasks, including `,
+    points: ["- installation and configuration computer hardware and software.",
+       "- troubleshooted computer problem", 
+       "- provided technical support to hospital staff",
+      "- maintained the hospital's IT infrastructure.",
+      "- Developing a training program for hospital staff on how to use the systems about related with their designation",
+      "I learned a lot during my time at the hospital and I gained valuable experience in a variety of IT disciplines. "]
+  },
+
+];
+
+export const PROJECTS = [
+  {
+    title: "Sales Management System",
+    image: project1,
+    description:
+      "Sales Management System is a e-commerce website with fully functional features like product listing, shopping cart and user authentication.",
+    technologies: ["C#", "ASP.NET", "Microsoft SQL Server", "IIS"],
+  },
+  {
+    title: "E-Bridge",
+    image: project2,
+    description:
+      "E-Bridge is a mobile app that connects teachers and students for seamless learning. It features chat, video conferencing, audio calls, notes sharing, and file transfers, making online classes interactive and efficient.",
+    technologies: ["Java", "Android Studio", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["React", "TailwindCSS", "Framer Motion"],
+  },
+  {
+    title: "Laser Security System using Arduino",
+    image: project5,
+    description:
+      "This project designs a laser-based security system using an LDR sensor and buzzer. When the laser beam is interrupted, the sensor detects it and triggers an alarm, ensuring efficient and reliable intrusion detection.",
+    technologies: ["C++", "Arduino"],
+  },
+  {
+    title: " Image Filtering",
+    image: project6,
+    description:
+      "This project explores image filtering techniques using Tkinter, PIL and OpenCV. Users can apply grayscale, negative, edge detection, noise reduction and rotation.",
+    technologies: ["Python"],
+  },
+  {
+    title: " Online Multiplayer Shooting Game",
+    image: project7,
+    description:
+      "This first-person shooter game, built with Unity and Photon PUN2, features real-time multiplayer, lobby creation, and competitive free-for-all gameplay.",
+    technologies: ["C#", "Photon Pun2", "Unity Engine"],
+  },
+];
+
+export const CONTACT = {
+  address: "Athokpam Awang Leikai, Thoubal - 795138",
+  phoneNo: "+91-7629938972 ",
+  email: "gripsonthongam@gmail.com",
+};
