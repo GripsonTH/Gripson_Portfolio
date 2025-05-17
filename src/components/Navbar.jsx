e6b198524f47706fc8dfa1e6cedd403cc1e5b8b5
GripsonTH <gripsonthongam@gmail.com>
2025-05-17 19:10:46 +0530

    fix download

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import logo from "../assets/GTLogo.png";
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import Resume from "../assets/Resume.png"
+import Resume from "../assets/Resume.png";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <a href="/"><img className="mx-2 w-20" src={logo} alt="logo" /></a>
+        <a href="/">
+          <img className="mx-2 w-20" src={logo} alt="logo" />
+        </a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a
@@ -32,7 +34,13 @@ const Navbar = () => {
         >
           <FaInstagram className="hover:text-pink-500 transition duration-200" />
         </a>
-        <a href="/Resume_May.pdf" download="Resume_May.pdf"><img className="w-7 hover:scale-105 hover:rotate-3 hover:opacity-75 transition duration-200" src={Resume} alt="Resume" /></a>
+        <a href="/Gripson_Portfolio/GripsonResume.pdf" download="GripsonResume.pdf">
+          <img
+            className="w-7 hover:scale-105 hover:rotate-3 hover:opacity-75 transition duration-200"
+            src={Resume}
+            alt="Resume"
+          />
+        </a>
       </div>
     </nav>
   );
